47b48bb4ab72762639d605132c57fd1e51d492ed
Sourabh Maheshwari <maheshwari.saurabh2@gmail.com>
2016-06-15 05:10:59 +0000

    using mongo remote and adding angular-resource

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,5 @@
-angular.module('app', []);
+angular.module('app', ['ngResource']);
 
-angular.module('app').controller('testCtrl', function($scope){
-    $scope.jobs = [{
-        title: 'Sales Person',
-        description: 'You will fight dragons'
-    },{
-        title: 'Software Developer',
-        description: 'You will provide weapons'
-    }];
+angular.module('app').controller('testCtrl', function($scope, $resource){
+    $scope.jobs = $resource('/api/jobs').query();
 });
