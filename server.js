4a9f7b6a16d981081540f55fa3d82e6dd092823e
Sourabh Maheshwari <maheshwari.saurabh2@gmail.com>
2016-06-15 05:05:06 +0000

    using mongo remote

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job.js');
 
 var app = express();
 
@@ -6,8 +8,23 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname+'/public'));
 
+
+app.get('/api/jobs', function(req, res){
+    mongoose.model('Job').find({}).exec(function(err, collection){
+        res.send(collection);
+    });
+})
 app.get('*', function(req, res){
     res.render('index');
+});
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://jobfinderuser:jobfinderuser@ds015584.mlab.com:15584/jobfinder');
+
+var con = mongoose.connection;
+con.once('open', function(){
+    console.log('connection to mongodb successful.');
+    jobModel.seedJobs();
 });
 
 app.listen(process.env.PORT, process.env.IP);
